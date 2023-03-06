aa21708f40a77e090cc9eae69060109c391bc3be
Praveen077Kumar <pk077prince@gmail.com>
2023-03-06 11:43:57 +0530

    adding middleware functionality with express

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -1,5 +1,18 @@
 const http = require("http");
-const routes= require('./routes');
+const express= require('express')
+const app= express();
 
-const server= http.createServer(routes);
-server.listen(3050);
+//Adding the middleware methdology
+
+app.use((req,res,next)=>{
+    console.log("In the Middleware!")
+    next();      //Allow the request to continue to the next middleware in line.
+})
+
+app.use((req,res,next)=>{
+    console.log('In Another Middleware!')
+    res.send('<h1>Hello! From PRAVEEN KUMAR <h2>Using the Middleware Functionality</h2></h1>')
+})
+
+const server= http.createServer(app);
+server.listen(3030);
--- testingapp/app.js
+++
@@ -1,5 +0,0 @@
-http = require('http');
-const routes= require('./routes')
-
-const server=http.createServer(routes)
-server.listen(3050);
