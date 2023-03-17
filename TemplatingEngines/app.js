1498177018f2a46d31fca6551372d90c952307d0
Praveen077Kumar <pk077prince@gmail.com>
2023-03-17 18:02:33 +0530

    adding the active class and dynamic pagetitle

--- TemplatingEngines/app.js
+++ TemplatingEngines/app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404',{pageTItle:'Page not found!'});
 });
 
 app.listen(3030);
