e532342e647afc15b9ecb568a18035e5473d4c17
Praveen077Kumar <pk077prince@gmail.com>
2023-03-23 01:06:21 +0530

    fetching data from files via the models

--- Model-View-Controller/models/product.js
+++ Model-View-Controller/models/product.js
@@ -1,15 +1,38 @@
-const products= [];
+const fs = require("fs");
+const path = require("path");
 
-module.exports= class Product{
-    constructor(t){
-        this.title=t
-    }
-
-    save(){
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p,(err,fileContent)=>{
+        let products=[];
+        if(!err){
+            products=JSON.parse(fileContent);
+        }
         products.push(this);
-    }
-
-    static fetchAll(){
-        return products
-    }
-}
+        fs.writeFile(p,JSON.stringify(products),(err)=>{
+            console.log(err);
+        });
+    });
+  }
+  static fetchAll(callback) {
+    const p = path.join(
+        path.dirname(process.mainModule.filename),
+        "data",
+        "products.json"
+      );
+    fs.readFile(p,(err, fileContent)=>{
+        if(err){
+            callback([]);
+        }
+        return callback(JSON.parse(fileContent)); 
+    });
+  }
+};
