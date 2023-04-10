fdf6f07b671778a4440bb5995c60836523bd4057
Praveen077Kumar <pk077prince@gmail.com>
2023-04-10 16:52:39 +0530

    adding user model and adding one-to-many-relationship

--- DataBase_with_sequelize/app.js
+++ DataBase_with_sequelize/app.js
@@ -1,10 +1,12 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error');
+const Product= require('./models/product');
+const User= require('./models/user');
 
+
+const errorController = require('./controllers/error');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,8 +23,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constaints: true,onDelete:'CASCADE' });
+User.hasMany(Product)
+
 const sequelize = require('./util/database')
-sequelize.sync().then(result =>{
+sequelize.sync({force:true}).then(result =>{
     app.listen(3000);
 }).catch(err => {
     console.log(err)
