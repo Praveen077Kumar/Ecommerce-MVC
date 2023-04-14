af1a3f932b7ac0eff0754110215053239401fb77
Praveen077Kumar <pk077prince@gmail.com>
2023-04-14 17:12:31 +0530

    creating and managing the dummy user

--- DataBase_with_sequelize/app.js
+++ DataBase_with_sequelize/app.js
@@ -1,13 +1,11 @@
 const path = require('path');
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 
 const Product= require('./models/product');
 const User= require('./models/user');
-
-
 const errorController = require('./controllers/error');
-const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -17,19 +15,36 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req, res,next) =>{
+    User.findByPk(1)
+    .then((user) =>{
+        req.user = user;
+        next();
+    })
+    .catch((err) => console.log(err));
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+
 Product.belongsTo(User, { constaints: true,onDelete:'CASCADE' });
 User.hasMany(Product)
 
 const sequelize = require('./util/database')
-sequelize.sync({force:true}).then(result =>{
+sequelize.sync().then(result =>{
+   return User.findByPk(1);
+}).then(user =>{
+    if(!user){
+        return User.create({name:'Max',email:'Max1234@gmail.com'})
+    }
+    return user;
+})
+.then(user =>{
     app.listen(3000);
-}).catch(err => {
+})
+.catch(err => {
     console.log(err)
 });
 
