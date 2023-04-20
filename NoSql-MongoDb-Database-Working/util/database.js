825d267d687466cd27679970bdb4dadac09e0108
PRAVEEN KUMAR <pk077prince@gmail.com>
2023-04-20 15:52:44 +0530

    Update database.js

--- NoSql-MongoDb-Database-Working/util/database.js
+++ NoSql-MongoDb-Database-Working/util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect=callback => {
     MongoClient.connect(
-        "mongodb+srv://praveen:hJadYASdmnfbICMN@praveencon.ccmwx.mongodb.net/?retryWrites=true&w=majority"
+        "mongodb+srv://<username>:<password>@praveencon.cmbxw.mongodb.net/?retryWrites=true&w=majority"
       )
         .then((result) => {
           console.log("Connected successfully");
