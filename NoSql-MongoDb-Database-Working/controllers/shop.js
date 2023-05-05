bad4d48958e780465cd2a2e4584ab4bf8964ee08
Praveen077Kumar <pk077prince@gmail.com>
2023-05-05 17:53:02 +0530

    Displaying Cart and Adding the Deletion functionality

--- NoSql-MongoDb-Database-Working/controllers/shop.js
+++ NoSql-MongoDb-Database-Working/controllers/shop.js
@@ -53,17 +53,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
+    .then(products => {
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
             products: products
-          });
         })
-        .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
 };
@@ -72,7 +67,8 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-      return req.user.addToCart(product);
+      req.user.addToCart(product);
+      res.redirect('/cart')
     })
     .then(result => {
       console.log(result);
@@ -112,14 +108,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
+    .deleteItemFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
