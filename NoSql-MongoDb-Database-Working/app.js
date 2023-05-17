1bc120616493392eede8a8ccc34ffa114f3fe90a
Praveen077Kumar <pk077prince@gmail.com>
2023-05-17 16:00:14 +0530

    using the session middleware

--- NoSql-MongoDb-Database-Working/app.js
+++ NoSql-MongoDb-Database-Working/app.js
@@ -2,9 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -18,11 +19,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
-  User.findById('645357bfc282a8874da5bd9a')
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -34,6 +38,25 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://praveen:2haSN3BrOaWBxJbq@praveencon.ccmwx.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Praveen',
+          email: 'princepk077@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- NoSql-MongoDb-Database-Working/controllers/auth.js
+++ NoSql-MongoDb-Database-Working/controllers/auth.js
@@ -1,6 +1,18 @@
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', {
-      path: '/login',
-      pageTitle: 'Login'
-    });
-  };
+  //   const isLoggedIn = req
+  //     .get('Cookie')
+  //     .split(';')[1]
+  //     .trim()
+  //     .split('=')[1] === 'true';
+  console.log(req.session.isLoggedIn);
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  req.session.isLoggedIn = true;
+  res.redirect('/');
+};
