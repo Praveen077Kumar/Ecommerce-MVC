ae39260b6bd07a56f56f4a753b8babeb9a969208
Praveen077Kumar <pk077prince@gmail.com>
2023-06-05 16:19:49 +0530

    Encrypt Passwords

--- Authentication_In_Node/controllers/auth.js
+++ Authentication_In_Node/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt= require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -36,13 +37,16 @@ exports.postSignup = (req, res, next) => {
   .then((userDoc) => {
     if(userDoc) {
       return res.redirect('/signup');
-    };
+    }
+    return bcrypt.hash(password, 12);
+  })
+  .then(hashedPassword =>{
     const user=  new User({
       email: email,
-      password: password,
+      password: hashedPassword,
       cart: { items: [] }
 
-    });
+    });    
     return user.save();
   })
   .then(result =>{
