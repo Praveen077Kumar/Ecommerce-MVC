89b76020333b0077190e7ffa211ec4d7590335e1
Praveen077Kumar <pk077prince@gmail.com>
2023-06-08 12:36:12 +0530

    adding csrf token and fixing the order button and cart route

--- Authentication_In_Node/controllers/auth.js
+++ Authentication_In_Node/controllers/auth.js
@@ -1,5 +1,6 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
-const bcrypt= require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -20,27 +21,30 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  
-  User.findOne({ email: email})
+  User.findOne({ email: email })
     .then(user => {
-      if(!user) {
+      if (!user) {
         return res.redirect('/login');
       }
       bcrypt
-      .compare(password, user.password)
-      .then(doMatch => {
-        if(doMatch) {
-          req.session.isLoggedIn = true;
-          req.session.user = user;
-          return  req.session.save(err => {
-            console.log(err);
-            res.redirect('/');
-          });
-        }
-        res.redirect('/login')
-      });
-     
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
+    .catch(err => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
@@ -48,27 +52,28 @@ exports.postSignup = (req, res, next) => {
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
   User.findOne({ email: email })
-  .then((userDoc) => {
-    if(userDoc) {
-      return res.redirect('/signup');
-    }
-    return bcrypt.hash(password, 12)
-    .then(hashedPassword =>{
-      const user=  new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] }
-  
-      });    
-      return user.save();
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+        });
     })
-    .then(result =>{
-      res.redirect('/login');
-    })
-  }).catch(err => {
-    console.log(err)
-  })
-}
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
--- Authentication_In_Node/routes/admin.js
+++ Authentication_In_Node/routes/admin.js
@@ -1,20 +1,20 @@
 const path = require('path');
-const isAuth= require('../middleware/isAuth')
 
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', isAuth,adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', isAuth,adminController.postAddProduct);
+router.post('/add-product', isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
