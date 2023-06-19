19078477880987c7eaafd1e701442d81bc3df01e
Praveen077Kumar <pk077prince@gmail.com>
2023-06-19 17:23:55 +0530

    setup the basic validation

--- Authentication_In_Node/routes/auth.js
+++ Authentication_In_Node/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
-
+const {check} = require('express-validator')
 const authController = require('../controllers/auth');
-
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
@@ -10,7 +9,7 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',check('email').isEmail(), authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
